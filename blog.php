947f139e94818fac6de914b23d13c577b21655a1
lalitkumar790 <60656194+lalitkumar790@users.noreply.github.com>
2022-05-18 22:02:59 +0530

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,71 @@
+<!--Header: site header with title, description & meta content-->
+<?php include 'inc/header.php'; ?>
+
+<!--MENU: navigation bar-->
+<?php include 'inc/menu.php'; ?>
+
+	<!-- First Post -->
+	<div id="grey">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<p><img src="assets/img/user.png" width="50px" height="50px"> <ba>Lalit sikarwar</ba></p>
+					<p><bd>March 18, 2022</bd></p>
+					<h4>The baby care</h4>
+					<p><b>kids</b> 
+					Clothing and Layette.
+Diapers.
+Bath Items.
+Grooming/First Aid Tools.
+Bedding and Sleep Needs.
+Feeding Supplies. </p>
+					<p>Show your love. Remember to praise your child. ...
+Create a routine. Structure — such as regularly scheduled meals and bedtimes — helps your child know what to expect.
+Find quality child care. ...
+Set limits. ...
+Don't feel guilty. ...
+Take care of yourself. ...
+Lean on others. ...
+Stay positive.</p>
+					<p><a href="blog01.php">Continue Reading...</a></p>
+				</div>
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /grey -->
+	
+	<!-- Second Post -->
+	<div id="white">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<p><img src="assets/img/user.png" width="50px" height="50px"> <ba>Lalit sikarwar</ba></p>
+					<p><bd>March 3, 2022</bd></p>
+					<h4>An Image Post</h4>
+					<p><img class="img-responsive" src="assets/img/blog01.jpg" alt="" style="height:150px; width:200px;">
+					<p>how is this product?.</p>
+					<p><a href="blog01.php">Continue Reading...</a></p>
+				</div>
+
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /white -->
+	
+	<!-- Third Post -->
+	<div id="grey">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<p><img src="assets/img/user.png" width="50px" height="50px"> <ba>Lalit sikarwar</ba></p>
+					<p><bd>march 01, 2022</bd></p>
+					<h4>Believe In Yourself</h4>
+					<p class="bq">"A bird sitting on a tree is never afraid of the branch breaking, because her trust is not in the branch, but in her own wings."</p>
+					<p><a href="blog01.php">Continue Reading...</a></p>
+				</div>
+
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /grey -->
+	
+	
+<!--Footer: footer navigation & social link -->
+<?php include 'inc/footer.php'; ?>	
---
+++ blog01.php
@@ -0,0 +1,30 @@
+<!--Header: site header with title, description & meta content-->
+<?php include 'inc/header.php'; ?>
+
+<!--MENU: navigation bar-->
+<?php include 'inc/menu.php'; ?>
+
+	<!-- Post -->
+	<div id="white">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<p><img src="assets/img/user.png" width="50px" height="50px"> <ba>Stanley Stinson</ba></p>
+					<p><bd>January 3, 2014</bd></p>
+					<h4>An Image Post</h4>
+					<p><img class="img-responsive" src="assets/img/blog01.jpg" alt=""></p>
+					<p>Kids must feel safe and sound, with their basic survival needs met: shelter, food, clothing, medical care and protection from harm.</p>
+					<p>The crib or bassinet should be free of loose-fitting sheets, bumpers, blankets, pillows, wedges, positioners, and stuffed animals. In short, nothing other than your baby and a pacifier are allowed. Yes, a pacifier is fair game and may even reduce the risk of SIDS.</p>
+					<p>An adequate supply of diapers is a basic need for all infants, as necessary for health and well-being as food and shelter.</p>
+					<br>
+				<!--	<p><bt>TAGS: <a href="#">Wordpress</a> - <a href="#">Web Design</a></bt></p>-->
+					<hr>
+					<p><a href="blog.php"># Back</a></p>
+				</div>
+
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /white -->
+
+<!--Footer: footer navigation & social link -->
+<?php include 'inc/footer.php'; ?>
